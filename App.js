1497ca17b4e6b4c3198e9c4c8513583e643ef07c
Min Li <limin9@gmail.com>
2017-11-10 15:09:17 +0800

    #1: implement deck list view and deck view

--- App.js
+++ App.js
@@ -1,14 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button,TouchableOpacity } from 'react-native';
+import { StackNavigator } from 'react-navigation'
+
+
+const Decks= ({navigation})=>{
+  //key is required to solve warning: "VirtualizedList: missing keys for items"
+  const data=[
+    {key:"1",title:"java",cardCount:3},
+    {key:"2",title:"javascript",cardCount:0},
+    {key:"3",title:"python",cardCount:6},
+  ]
+
+  const Deck=({title,cardCount})=>{
+    return (
+      <View>
+        <TouchableOpacity onPress={()=>navigation.navigate('Deck',{title,cardCount})}>
+          <Text>{title}</Text>
+        </TouchableOpacity>
+        <Text>{cardCount}</Text>
+      </View>
+    )
+  }
+
+  const renderItem=({item})=>{
+    return <Deck {...item}/>
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <FlatList data={data} renderItem={renderItem}/>
+    </View>
+  )
+}
+
+const Deck=({navigation})=>{
+  return (
+    <View>
+      <Text>{navigation.state.params.title}</Text>
+      <Text>{navigation.state.params.cardCount}</Text>
+      <Button title="Add Card" onPress={()=>alert('Add Card')}/>
+      <Button title="Start Quiz" onPress={()=>alert('Start Quiz')}/>
+    </View>
+  )
+}
+
+const Stack = StackNavigator({
+  Decks:{
+    screen:Decks,
+    navigationOptions:({navigation})=>({
+        title:"DECKS"
+    })
+  },
+  Deck:{
+    screen:Deck,
+    navigationOptions:({navigation})=>({
+      title:navigation.state.params.title
+    })
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Stack/>
     );
   }
 }
