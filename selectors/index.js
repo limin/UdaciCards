0e05bddf8fb905675f8c04a85a7f3548909d00da
Min Li <limin9@gmail.com>
2017-11-18 16:55:40 +0800

    return new deck object to make the selector as pure function, otherwise the deckdetail ui won't be updated when the cards count is changed

--- selectors/index.js
+++ selectors/index.js
@@ -1,7 +1,8 @@
 import { createSelector } from 'reselect'
 
 export const getDerivedDecks=createSelector([state=>state.decks,state=>state.cards],(decks,cards)=>{
-  return Object.values(decks).map((deck)=>{
+  return Object.values(decks).map((d)=>{
+    const deck=Object.assign({},d)
     deck.cards=Object.values(cards).filter((card)=>card.deckId===deck.id)
     return deck
   })
