fa8e67a9b3682dce98859447fe8b0a2570621ef7
Min Li <limin9@gmail.com>
2017-11-18 16:03:50 +0800

    #10: multiple themes support

---
+++ theme/classic.js
@@ -0,0 +1,33 @@
+const classic={
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 8,
+  },
+  listHeader:{
+    height:2,
+    backgroundColor:'#fff'
+  },
+  listFooter:{
+    height:2,
+    backgroundColor:'#fff'
+  },
+  listItemSeparator:{
+    height:1,
+    backgroundColor:'#eee'
+  },
+  buttonBar: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'flex-start'
+  },
+  button: {
+    flex: 0,
+    padding: 3,
+    margin: 3,
+    backgroundColor: '#4097ce',
+    color: '#fff'
+  }
+}
+
+export default classic
---
+++ theme/index.js
@@ -0,0 +1,7 @@
+import {StyleSheet} from 'react-native'
+import classic from './classic.js'
+
+const theme = StyleSheet.create({
+  ...classic
+});
+export default theme
