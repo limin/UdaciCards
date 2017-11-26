9ffddef4efa0d8865f1d6ecb767c68a1e73ab8e8
Min Li <limin9@gmail.com>
2017-11-26 20:44:49 +0800

    add 3 default cards

--- utils/API.js
+++ utils/API.js
@@ -1,123 +1,8 @@
 import { AsyncStorage } from 'react-native'
 import { uid } from '../utils'
+import {decks, cards} from './data.js'
 
 const STORE_KEY="org.udacicards.store"
-const DECKS={
-  "1":{
-    id:"1",
-    title:"Fun English word quizzes",
-    timestamp:Date.now(),
-  }
-}
-
-// from: http://iteslj.org/c/jokes-riddles.html
-const CARDS={
-  "11":{
-    id:"11",
-    question:"Why is nine afraid of seven?",
-    answer:"Because seven eight(ate) nine",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "12":{
-    id:"12",
-    question:"What did zero say to eight?",
-    answer:"Nice belt. (The 8 looks like a 0 with a belt around its waist.)",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "13":{
-    id:"13",
-    question:"What can't be used until it's broken?",
-    answer:"An egg.",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "14":{
-    id:"14",
-    question:"Why did the man throw the butter out the window? ",
-    answer:"He wanted to see the butterfly. ",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "15":{
-    id:"15",
-    question:"Do you know why birds fly to south in the winter?",
-    answer:"Because it's too far to walk there.",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "16":{
-    id:"16",
-    question:"Which room has no doors, no windows?",
-    answer:"A mushroom.",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "17":{
-    id:"17",
-    question:"A father and his son were in a car accident. The father died. The son was taken to the hospital. The doctor came in and said: I can't do surgery on him, because he's my son. Who was the doctor?",
-    answer:"The doctor was his mother.",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "18":{
-    id:"18",
-    question:"Why did the student take a ladder to school?",
-    answer:"Because he/she was going to high school!",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "19":{
-    id:"19",
-    question:"Why did the tomato blush?",
-    answer:"Beacuse it saw the salad dressing!",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "20":{
-    id:"20",
-    question:"What do you call a deer with no eyes?",
-    answer:"No idea. (No eye deer)",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "21":{
-    id:"21",
-    question:"What flowers have two lips?",
-    answer:"Tulips",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "22":{
-    id:"22",
-    question:"They travel all over the world but end up in the corner, what are they?",
-    answer:"Stamps",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "23":{
-    id:"23",
-    question:"What's a teacher's favorite nation?",
-    answer:"Expla-nation.",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "24":{
-    id:"24",
-    question:"What is the longest word in the English language?",
-    answer:"Smiles. (There is a mile between the first letter and the last letter.) ",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-  "25":{
-    id:"25",
-    question:"What did the fish say when he hit the wall?",
-    answer:"Dam!",
-    deckId:"1",
-    timestamp:Date.now(),
-  },
-}
 
 /**
 sample quiz
@@ -141,7 +26,7 @@ export const getData=()=>{
   return AsyncStorage.getItem(STORE_KEY).then((value)=>{
     let data={}
     if(value==null){
-      data={decks:DECKS,cards:CARDS,quizzes:{}}
+      data={decks,cards,quizzes:{}}
     }else{
       data=JSON.parse(value)
     }
---
+++ utils/data.js
@@ -0,0 +1,138 @@
+export const decks={
+  "1":{
+    id:"1",
+    title:"Fun English word quizzes",
+    timestamp:Date.now(),
+  }
+}
+
+// from: http://iteslj.org/c/jokes-riddles.html
+export const cards={
+  "11":{
+    id:"11",
+    question:"Why is nine afraid of seven?",
+    answer:"Because seven eight(ate) nine",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "12":{
+    id:"12",
+    question:"What did zero say to eight?",
+    answer:"Nice belt. (The 8 looks like a 0 with a belt around its waist.)",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "13":{
+    id:"13",
+    question:"What can't be used until it's broken?",
+    answer:"An egg.",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "14":{
+    id:"14",
+    question:"Why did the man throw the butter out the window? ",
+    answer:"He wanted to see the butterfly. ",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "15":{
+    id:"15",
+    question:"Do you know why birds fly to south in the winter?",
+    answer:"Because it's too far to walk there.",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "16":{
+    id:"16",
+    question:"Which room has no doors, no windows?",
+    answer:"A mushroom.",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "17":{
+    id:"17",
+    question:"A father and his son were in a car accident. The father died. The son was taken to the hospital. The doctor came in and said: I can't do surgery on him, because he's my son. Who was the doctor?",
+    answer:"The doctor was his mother.",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "18":{
+    id:"18",
+    question:"Why did the student take a ladder to school?",
+    answer:"Because he/she was going to high school!",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "19":{
+    id:"19",
+    question:"Why did the tomato blush?",
+    answer:"Beacuse it saw the salad dressing!",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "20":{
+    id:"20",
+    question:"What do you call a deer with no eyes?",
+    answer:"No idea. (No eye deer)",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "21":{
+    id:"21",
+    question:"What flowers have two lips?",
+    answer:"Tulips",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "22":{
+    id:"22",
+    question:"They travel all over the world but end up in the corner, what are they?",
+    answer:"Stamps",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "23":{
+    id:"23",
+    question:"What's a teacher's favorite nation?",
+    answer:"Expla-nation.",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "24":{
+    id:"24",
+    question:"What is the longest word in the English language?",
+    answer:"Smiles. (There is a mile between the first letter and the last letter.) ",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "25":{
+    id:"25",
+    question:"What did the fish say when he hit the wall?",
+    answer:"Dam!",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "26":{
+    id:"26",
+    question:"I start with 'T' ends with 'T' and within me is 'T'. What am I?",
+    answer:"Teapot",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "27":{
+    id:"27",
+    question:"I'm start of Earth and the end of Universe. What am I?",
+    answer:"e",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+  "28":{
+    id:"28",
+    question:"I'm tall when I'm young and I'm short when I'm old, when I'm alive I glow, because of your breath I die. What am I?",
+    answer:"Candle",
+    deckId:"1",
+    timestamp:Date.now(),
+  },
+
+}
